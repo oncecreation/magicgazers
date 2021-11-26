a9c9595aad8ba8f510d57323152a1e367b551e6a
jameshussey474 <jameshussey474@gmail.com>
2021-11-26 12:10:37 +0530

    port error fix

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(process.env.PUBLIC_PORT, () => {
-	console.log(`Server ready ${process.env.PUBLIC_PORT}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Server ready ${process.env.PORT}`);
 });
